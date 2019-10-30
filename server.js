acc2a39378741b2fe74b6364d0152a985eb5779f
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-30 14:31:09 +0000

    Add posts endpoints

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 
 const server = express();
 
 server.use(express.json());
 
 server.use("/api/users", logger, userRouter);
+server.use("/api/posts", logger, postRouter);
 
 server.get("*", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
