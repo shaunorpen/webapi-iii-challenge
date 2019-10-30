fd54e91226269cce11a3b1f3dc465d88b56f529d
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-30 12:39:13 +0000

    Add middleware to log request method, url & tstamp

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("*", logger, (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
 function logger(req, res, next) {
-  // - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
-  // - this middleware runs on every request made to the API
-  console.log(req);
+  const { method, url } = req;
+  const timestamp = new Date();
+  console.log(
+    `*** NEW REQUEST ***\n Method: ${method}\n URL: ${url}\n Timestamp: ${timestamp}`
+  );
+  next();
 }
 
 module.exports = server;
